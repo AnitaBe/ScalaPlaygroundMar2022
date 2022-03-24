19a2a98854e0e7a40198a9e5bd932b9c69048422
Anita <anitaikaminskai@gmail.com>
2022-03-24 19:23:03 +0200

    update 22.03

--- src/main/scala/MyUtil.scala
+++ src/main/scala/MyUtil.scala
@@ -3,7 +3,7 @@ object MyUtil {
   def myRound(n: Double, precision: Int=0): Double = {
     //trick is to multiply by some power of 10 then divide by that multiplier
     //so (n*100).round/100 would give us 2 points of precision
-    0.0 //FIXME
+    0.0 //
     val multiplier = Math.pow(10, precision)
     (n*multiplier).round/multiplier  //we utilize built in round
     //n.round
--- src/main/scala/TreesAssignment.scala
+++ src/main/scala/TreesAssignment.scala
@@ -1,28 +1,43 @@
+import Day5WhileLoops.{counter, i}
+
 import scala.io.StdIn.readLine
 
 object TreesAssignment extends App {
-  //TODO ask person's name
+  // ask person's name
 
-  val humanName=readLine("What is your name? ")
+  val humanName = readLine("What is your name?\n ").toUpperCase()
 
-  //TODO ask for tree height
+  // ask for tree height
   val maxHeight = 40
   val minHeight = 3
-  val treeHeight = readLine("What is height of your tree? ").toInt
-  for ((c, i)<- humanName.zipWithIndex) {
-    if (treeHeight < minHeight) {
-      println(s"Minimum height should be more than $minHeight")
-    }
-    else {
-      println(" " * (humanName.length - i) + s"${c}" * (i + 2))
-    }
+  val treeHeight = readLine("What is height of your tree?\n ").toInt
+
+  // for (c <- 0 to treeHeight) println(" "*(treeHeight-c)+"*"+"**"*c)
+  for (c <- 0 to treeHeight-1) {
+    print(" " * (treeHeight - c))
+    for (b <- 1 to 1 + 2 * c) {
+      if (c==0) {print("*")
+    } else {print(humanName.charAt((c-1)%humanName.length))}
   }
+    println()
+}
+
+  //another version
+  //for ((c,i)<- humanName.zipWithIndex) {
+  //if (treeHeight < minHeight) {
+  //println(s"Minimum height should be more than $minHeight")
+  // }
+ // else { if (i==0) { println(" " * (treeHeight - i) +"*")}
+  //   else println(" " * (treeHeight - i) + s"${c}"+ (s"${c}"+s"${c}") * (i))
+      //println(" " * (treeHeight - i) + s"${c}" * (i + 2))
+   // }
+  //}
 
 
 
 
 
-  //TODO print a xmas tree (or another tree) by calling printTree function with the correct parameters
+  //TODOprint a xmas tree (or another tree) by calling printTree function with the correct parameters
 
 
   //tree height should be the one assigned
@@ -48,6 +63,7 @@ object TreesAssignment extends App {
 
   def printTree(name:String, height:Int, symbol:Char='*', maximumHeight:Int=40):Unit = {
     //for this exercise we are not going to adjust maximumHeight
-    //TODO print the tree HINT: check the last entry in Day7Strings
+    // print the tree HINT: check the last entry in Day7Strings
+
   }
 }
